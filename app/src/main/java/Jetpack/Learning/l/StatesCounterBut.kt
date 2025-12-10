d24f64b743358420b552b0cf4c5015d81666cf90
Jakhongir <sakatazohon@gmail.com>
2025-12-10 19:41:37 +0500

    дома доделаю

--- app/src/main/java/Jetpack/Learning/l/StatesCounterBut.kt
+++ app/src/main/java/Jetpack/Learning/l/StatesCounterBut.kt
@@ -1,20 +1,31 @@
 package Jetpack.Learning.l
 
+import android.content.ContentValues
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -42,3 +53,24 @@ fun Increment(value:Int,onClickChange:(Int)->Unit){
 
 }
 val localParams= compositionLocalOf{}
+var username by remember { mutableStateOf("") }
+
+
+
+@Composable
+fun TextFieladCheck(){
+   TextField(value=username,
+       onValueChange = {username=it},
+       modifier = Modifier.size(width = 150.dp, height = 50.dp),
+       placeholder = {Text("Поле для ввода") },//Это текст-подсказка, который отображается, пока пользователь ничего не ввёл.
+       leadingIcon = { Icon(Icons.Default.Person, contentDescription = null) }, //Иконка слева от текста.
+       trailingIcon = { Icon(Icons.Default.Clear, contentDescription = null) }, //Иконка справа от текста.
+       prefix = { Text("Text:") }, //Компонент перед вводимым текстом, внутри поля.
+       suffix = { Text("| here is the end of text") }, //Компонент после вводимого текста, внутри поля.
+       supportingText = { Text("Пароль должен быть минимум 8 символов") } , //Текст под полем, который может давать подсказку или сообщение об ошибке.
+       visualTransformation = PasswordVisualTransformation(),
+
+
+   )
+
+}
