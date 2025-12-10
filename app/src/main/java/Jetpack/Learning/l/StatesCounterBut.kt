6eba2713ba371d5a2e7ff1226cd1f1fb61e78496
Jakhongir <sakatazohon@gmail.com>
2025-12-10 18:02:27 +0500

    to hime check

--- app/src/main/java/Jetpack/Learning/l/StatesCounterBut.kt
+++ app/src/main/java/Jetpack/Learning/l/StatesCounterBut.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -39,3 +41,4 @@ fun Increment(value:Int,onClickChange:(Int)->Unit){
         modifier = Modifier.clickable(onClick = {onClickChange(value+1)}))
 
 }
+val localParams= compositionLocalOf{}
