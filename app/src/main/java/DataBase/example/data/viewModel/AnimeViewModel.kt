64ae0e8fa1e6c1d8e5c336979479c10a1c282c64
user <user@MacBook-Pro-user.local>
2026-02-28 12:22:59 +0500

    AimeVM

--- app/src/main/java/DataBase/example/data/viewModel/AnimeViewModel.kt
+++ app/src/main/java/DataBase/example/data/viewModel/AnimeViewModel.kt
@@ -1,4 +1,45 @@
 package DataBase.example.data.viewModel
 
-class AnimeViewModel {
+import DataBase.example.data.domain.repository.AnimeRepository
+import DataBase.example.data.domain.state.server.AnimeFullUiState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+
+@HiltViewModel
+class AnimeViewModel @Inject constructor(
+    private val repository: AnimeRepository
+): ViewModel(){
+    private val _state= MutableStateFlow(AnimeFullUiState())
+    val state=_state.asStateFlow()
+
+    private fun loadFullAnimeInfo(id:Int){
+        viewModelScope.launch {
+            _state.update { it.copy( isLoading = true, error = null) }
+
+            repository.getAnimeFullInfo(id).fold(
+                onSuccess = { response ->
+                    _state.update { it.copy(
+                        anime = response, // данные из репо
+                        isLoading = false
+                    )
+                    }
+                },
+                onFailure = { throwable ->
+                    _state.update { it.copy(
+                        error = throwable.message ?: "Unknown error",
+                        isLoading = false
+                    )
+                    }
+                }
+            )
+        }
+    }
 }
--- app/src/main/java/DataBase/example/data/viewModel/EpisodeListViewModel.kt
+++ app/src/main/java/DataBase/example/data/viewModel/EpisodeListViewModel.kt
@@ -1,4 +1,25 @@
 package DataBase.example.data.viewModel
 
-class EpisodeListViewModel {
+import DataBase.example.data.domain.repository.AnimeRepository
+import DataBase.example.data.domain.state.server.AnimeEpisodesUiState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class EpisodeListViewModel  @Inject constructor(
+    private val repository: AnimeRepository
+): ViewModel(){
+    private val _state= MutableStateFlow(AnimeEpisodesUiState())
+    private val state=_state.asStateFlow()
+
+    fun LoadEpisode(){
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+
+        }
+    }
 }
